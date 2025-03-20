80dc42d4e9010b75878e95478ecc365adc2582dc
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-20 11:46:01 -0500

    init

---
+++ src/test/java/com/isai/api_gestion_empleados/ApiGestionEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.api_gestion_empleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
